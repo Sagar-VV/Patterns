911e806288ec8cd8f2a385cf7d1c5fa64a42ae46
Sagar-VV <78678275+Sagar-VV@users.noreply.github.com>
2021-02-08 12:11:57 +0530

    Update Pattern_25.py

--- Pattern_25.py
+++ Pattern_25.py
@@ -1,4 +1,8 @@
-
+#           1 
+#         2 2 
+#       3 3 3                This was the required output, Please find the code below for the same.
+#     4 4 4 4 
+#   5 5 5 5 5 
 def numpat(n):
 
     for i in range(n,0,-1):
